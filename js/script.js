593a306e158b1f73e5682c6a236c5ced58f56ecb
Martinjens <93524321+Martinjens@users.noreply.github.com>
2023-01-27 13:08:14 +0100

    all the work

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
+const dropdownList = [...dropdownElementList].map(
+  (dropdownToggleEl) => new bootstrap.Dropdown(dropdownToggleEl)
+);
+
+const exampleModal = document.getElementById("exampleModal");
+exampleModal.addEventListener("show.bs.modal", (event) => {
+  // Button that triggered the modal
+  const button = event.relatedTarget;
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute("data-bs-whatever");
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  const modalTitle = exampleModal.querySelector(".modal-title");
+  const modalBodyInput = exampleModal.querySelector(".modal-body input");
+
+  modalTitle.textContent = `New message to ${recipient}`;
+  modalBodyInput.value = recipient;
+});
